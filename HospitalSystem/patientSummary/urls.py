10db5003e6ac012c35fefa1c5962d246d95a4f3a
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-10-06 23:05:38 +0300

    add doctor dashboard and fix some bugs

--- HospitalSystem/patientSummary/urls.py
+++ HospitalSystem/patientSummary/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("add/patient/summary/<appointment_id>/" ,views.add_patient_summary, name="add_patient_summary"),
     path("patient/summary/<summary_id>/" ,views.display_patient_summary, name="display_patient_summary"),
     path("all/patient/summary/" ,views.all_patient_summary, name="all_patient_summary"),
+    path("all/doctor/patient/summary/" ,views.all_doctor_patient_summary, name="all_doctor_patient_summary"),
 ]
