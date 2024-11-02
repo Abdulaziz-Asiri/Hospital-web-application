a0dd0ccc618e7f8fa1842deacc6e3c3c2f93602b
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-11-02 19:31:59 +0300

    write app name in installed apps

--- HospitalSystem/settings.py
+++ HospitalSystem/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_browser_reload",
+    'HospitalSystem',
     'main',
     'account',
     'clinic',
